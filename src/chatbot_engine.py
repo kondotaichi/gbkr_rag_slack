1c69cdeccab7ad6a2dc332fcac69e212c064d5f0
kondotaichi <ubjdij210@gmail.com>
2024-08-22 12:58:33 +0900

    名前情報

--- src/chatbot_engine.py
+++ src/chatbot_engine.py
@@ -1,4 +1,4 @@
-import langchain
+import os
 from langchain.chat_models import ChatOpenAI
 from langchain.document_loaders import DirectoryLoader
 from langchain.indexes import VectorstoreIndexCreator
@@ -18,38 +18,39 @@ from langchain.agents import AgentType
 
 langchain.verbose = True
 
-
 def create_index() -> VectorStoreIndexWrapper:
     from unstructured.file_utils.filetype import EXT_TO_FILETYPE, FileType
- 
+
     EXT_TO_FILETYPE[".py"] = FileType.TXT
- 
-    loader = DirectoryLoader("./src/", glob="**/*.py")
+
+    # すべてのファイルタイプをインデックスに含める
+    loader = DirectoryLoader("./src/", glob="**/*.*")
     return VectorstoreIndexCreator().from_loaders([loader])
 
-#indexをもとにtoolを作成
-def create_tools(index: VectorStoreIndexWrapper)->List[BaseTool]:
-    vectorstore_info=VectorStoreInfo(
+# Indexをもとにtoolを作成
+def create_tools(index: VectorStoreIndexWrapper) -> List[BaseTool]:
+    vectorstore_info = VectorStoreInfo(
         name="gbkr_rag_slack",
-        description="source code of gbkr_rag_slack",
+        description="Source code and other files in gbkr_rag_slack",
         vectorstore=index.vectorstore
     )
-    toolkit=VectorStoreToolkit(vectorstore_info=vectorstore_info)
+    toolkit = VectorStoreToolkit(vectorstore_info=vectorstore_info)
     return toolkit.get_tools()
-    
+
 def chat(
     message: str, history: ChatMessageHistory, index: VectorStoreIndexWrapper
 ) -> str:
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
-    tools=create_tools(index)
+    tools = create_tools(index)
     
-    memory=ConversationBufferMemory(chat_memory=history,memory_key="chat_history",return_messages=True)
+    memory = ConversationBufferMemory(chat_memory=history, memory_key="chat_history", return_messages=True)
     
-    agent_chain=initialize_agent(
+    agent_chain = initialize_agent(
         tools,
         llm,
         agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
         memory=memory
     )
-    #返し方はドキュメントを確認
+    
+    # 入力メッセージに基づいてファイルの情報を検索し、応答を生成
     return agent_chain.run(input=message)
